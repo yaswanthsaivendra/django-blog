a40c8e2730a6ddef82f1cb43532eec7c58ae748d
yaswanthsaivendra <yaswanthsaivendra@gmail.com>
2021-11-18 23:58:11 +0530

    update

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -133,8 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
