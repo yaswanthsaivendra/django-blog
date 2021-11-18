1a71bd5e2af1a392aeb3f042772c7b50938fc0ba
yaswanthsaivendra <yaswanthsaivendra@gmail.com>
2021-11-19 00:06:44 +0530

    update

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -140,8 +140,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
