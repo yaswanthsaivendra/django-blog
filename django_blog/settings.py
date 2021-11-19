b50033bd893665b96894c4c6daa1085ee8687882
yaswanthsaivendra <yaswanthsaivendra@gmail.com>
2021-11-19 10:21:11 +0530

    update

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import environ
+import django_heroku
 
 
 
@@ -135,6 +136,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+django_heroku.settings(locals())
 
 
 
