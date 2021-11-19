50ab3e26e56932e92e9e301adf5fa5ad89ff2c4a
yaswanthsaivendra <yaswanthsaivendra@gmail.com>
2021-11-19 10:30:55 +0530

    update

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -161,7 +161,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env('HOST_EMAIL')
+# EMAIL_HOST_USER = env('HOST_EMAIL')
+EMAIL_HOST_USER = 'yaswanthsaivendra@gmail.com'
 EMAIL_HOST_PASSWORD = env('HOST_PASSWORD')
 
 
